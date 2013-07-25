8974afe7e1bb586c1f9bedcb677633cb1faae3ac
xray7224 <jessica@megworld.co.uk>
2013-07-25 16:43:41 +0100

    Oops, typo

--- pamrel/urls.py
+++ pamrel/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns("",
     url(r"^$", "pamrel.views.api", name="docs"),
     url(r"^licence$", "pamrel.views.licence", name="licence"),
-    url(r"^license$", "pamrel.views.license", name="licence"),
+    url(r"^license$", "pamrel.views.licence", name="licence"),
     url(r"^(?P<pid>\w+)$", "pamrel.views.paste", name="paste"),
 )
