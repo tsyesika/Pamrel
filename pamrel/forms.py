2a5415b210ece099d7afb463dc7f55d009e4ce3c
Jessica Tallon <jessica@megworld.co.uk>
2014-03-15 18:05:20 +0000

    oops forget some files

---
+++ pamrel/forms.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+from django import forms
+from pamrel import models
+
+from pygments import lexers
+
+class PasteForm(forms.ModelForm):
+	""" Form to create a paste """
+
+	language = forms.ChoiceField(
+		required=True,
+		choices=((None, 'Auto Detect'),)
+	)
+
+	class Meta:
+		model = models.Paste
+		fields = ["content"]
+
+	def __init__(self, *args, **kwargs):
+		super(PasteForm, self).__init__(*args, **kwargs)
+		# Load all the choices for the languages pygments currently supports
+		self.fields['language'].choices += ((lexer[1][0], lexer[0]) for lexer in lexers.get_all_lexers())
+		self.fields['content'].widget.attrs.update({'placeholder': '<code> ... </code>'})
+
+	def detect_language(self, data):
+		""" Detects the language of the paste """
+		try:
+			lexer = lexers.guess_lexer(data)
+		except Exception:
+			return "PlainText"
+
+		# for some unknown reason get_lexer_by_name(lexer.name) doesn't work >.>
+		# however get_lexer_by_name(lexer.aliases[0]) does.
+		return lexer.aliases[0]
+
+	def save(self, *args, **kwargs):
+		obj = super(PasteForm, self).save(*args, **kwargs)
+
+		# Set language.
+		if self.cleaned_data.get("language", None) is None:
+			obj.language = self.detect_language(self.cleaned_data["content"])
+		else:
+			obj.language = self.cleaned_data['language']
+
+		# If we don't know the language, don't try syntax highlighting (duh!)
+		if obj.language == "PlainText":
+			obj.syntax = False
+		else:
+			obj.syntax = True
+
+		obj.save()
+		return obj
